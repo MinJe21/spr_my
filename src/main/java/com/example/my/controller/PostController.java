d85323fe1814bfbcc9f144e3271ac1e479e3a17d
minje <minje9370@handong.ac.kr>
2025-07-09 20:43:09 +0900

    modify PostController to use PrincipalDetails in SpringSecurity assignment

--- src/main/java/com/example/my/controller/PostController.java
+++ src/main/java/com/example/my/controller/PostController.java
@@ -1,11 +1,13 @@
 package com.example.my.controller;
 
 import com.example.my.dto.PostDto;
+import com.example.my.security.PrincipalDetails;
 import com.example.my.service.PostService;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,30 +18,33 @@ import java.util.List;
 public class PostController {
     private final PostService postService;
 
-    public Long getReqUserId(HttpServletRequest request){
-        if(request.getAttribute("reqUserId") == null){
+    public Long getReqUserId(@AuthenticationPrincipal PrincipalDetails principalDetails){
+        if(principalDetails == null || principalDetails.getUser() == null || principalDetails.getUser().getUserId() == null){
             return null;
         }
-        return (Long) request.getAttribute("reqUserId");
+        return principalDetails.getUser().getUserId();
     }
 
     @PostMapping("/create")
-    public ResponseEntity<PostDto.CreateResPost> createPost(@RequestBody PostDto.CreateReqPost params, HttpServletRequest request) {
-        Long reqUserId = getReqUserId(request);
+    public ResponseEntity<PostDto.CreateResPost> createPost(@RequestBody PostDto.CreateReqPost params,
+                                                            @AuthenticationPrincipal PrincipalDetails principalDetails) {
+        Long reqUserId = getReqUserId(principalDetails);
         params.setUserId(reqUserId);
         System.out.println("요청한 유저 아이디: " + reqUserId);
         return ResponseEntity.ok(postService.createPost(params));
     }
 
     @GetMapping("/read")
-    public ResponseEntity<List<PostDto.ReadResPost>> readPost(@ModelAttribute PostDto.ReadReqPost params, HttpServletRequest request) {
+    public ResponseEntity<List<PostDto.ReadResPost>> readPost(@ModelAttribute PostDto.ReadReqPost params,
+                                                              @AuthenticationPrincipal PrincipalDetails principalDetails) {
         List<PostDto.ReadResPost> responseDtoList = postService.readPost(params);
         return ResponseEntity.ok(responseDtoList);
     }
 
     @PutMapping("/update")
-    public ResponseEntity<PostDto.UpdateResPost> updatePost(@RequestBody PostDto.UpdateServPost params, HttpServletRequest request) {
-        Long reqUserId = getReqUserId(request);
+    public ResponseEntity<PostDto.UpdateResPost> updatePost(@RequestBody PostDto.UpdateServPost params,
+                                                            @AuthenticationPrincipal PrincipalDetails principalDetails) {
+        Long reqUserId = getReqUserId(principalDetails);
         params.setReqUserId(reqUserId);
         System.out.println("요청한 유저 아이디: " + reqUserId);
         postService.updatePost(params);
